b49766a20b76946213d06174f50aea738b5786f1
hashi-02 <hassie0209@gmail.com>
2022-07-11 11:58:14 +0900

    4

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import World from "./world";
-const root = document.getElementById('root')
-const world = new World("Hello World!");
-world.sayHello(root)
+// import World from "./world";
+// const root = document.getElementById('root')
+// const world = new World("Hello World!");
+// world.sayHello(root)
+
+
+
+
+//04.関数の方ていぎ
+import { logMessage } from "./function/basic";
+logMessage("Hello , WOlrd!")
